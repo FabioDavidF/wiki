7ba2c60cf5c45643d02ccc88818ba76712e9fac3
Fábio David Freitas <fabio.david.contato@gmail.com>
2020-09-28 09:26:32 -0300

    Did the entrypages and the index page to link to said entries, having trouble with the search form

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-
 from . import views
+from . import util
+
+
+encyclopedia_list = util.list_entries()
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("entry/<title>/".lower(), views.encyclopedia, name='entry_title')
 ]
+
+
+
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 from . import util
 
 
@@ -8,3 +7,15 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def encyclopedia(request, title):
+    content = util.get_entry(title)
+    if content == None:
+        return render(request, 'encyclopedia/encyclopedia.html', {
+            'title': 'Entry not found',
+            'header': 'Entry not found',
+            'content': 'Sorry, that entry does not exist yet'
+        })
+    return render(request, 'encyclopedia/encyclopedia.html', {
+        'page_title': 'Wiki - ' + title.capitalize(),
+        'content': content
+    })
