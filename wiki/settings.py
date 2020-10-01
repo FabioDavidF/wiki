858cbc597019b44d636a8c34ae5ca92a81e6eb71
Fábio David Freitas <fabio.david.contato@gmail.com>
2020-10-01 01:54:24 -0300

    Converted markdown to html using  markdownfy

--- wiki/settings.py
+++ wiki/settings.py
@@ -27,10 +27,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+MARKDOWNIFY_STRIP = False
+MARKDOWNIFY_WHITELIST_TAGS = {
+ 'a', 'p', 
+ 'h1', 'h2', 'h3','h4', 'h5', 'h6', 'h7',
+ 'ul', 'li', 'span',
+}
 
 # Application definition
 
 INSTALLED_APPS = [
+    'markdownify',
     'encyclopedia',
     'django.contrib.admin',
     'django.contrib.auth',
